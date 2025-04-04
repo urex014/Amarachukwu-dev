df40b79a6f6f0e794680a92f4862ec4e8e57dd06
Amarachukwu Onuoha <amarachukwuonuohabars007@gmail.com>
2025-04-04 13:52:34 +0100

    added demacation

--- Portfolio/src/components/HomeInfo.jsx
+++ Portfolio/src/components/HomeInfo.jsx
@@ -5,13 +5,16 @@ import { arrow } from "../assets/icons";
 const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
-      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-        Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Amarachukwu</span>
-        👋
-        <br />
-        A Full stack developer from Nigeria 
-      </h1>
+      <div>
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+          Hi, I'm
+          <span className='font-semibold mx-2 text-white'>Amarachukwu</span>
+          👋
+          <br />
+          A Full stack developer from Nigeria <br/> <b>⬅️SCROLL SIDEWAYS➡️</b>
+        </h1>
+        
+      </div>
     );
 
   if (currentStage === 2) {
