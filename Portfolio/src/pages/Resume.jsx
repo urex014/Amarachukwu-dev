3e9210876cdf23d33ece2fb7798b3536851193ae
Amarachukwu <amarachukwuonuohabars007@gmail.com>
2025-07-20 22:42:08 +0100

    fix:resume download

--- Portfolio/src/pages/Resume.jsx
+++ Portfolio/src/pages/Resume.jsx
@@ -7,9 +7,9 @@ export default function Resume(){
             <h1 className="text-3xl font-bold text-center my-8">Resume</h1>
             <img src={resume} alt="Resume" className="w-full h-auto max-w-2xl mx-auto mb-8 rounded-lg shadow-lg" />
             <button onClick={()=>{
-                window.open('src/assets/resume.pdf','_blank')
-            }} className="btn">Download my resume here</button>
-            <p className="text-center pt-5">For more details, feel free to contact me.</p>
+                window.open('https://drive.google.com/file/d/1d1MFXucufh3hSRbtguaZ7JJUVJr3qxjm/view?usp=drive_link', '_blank');
+            }} className="btn text-center b-4">Download my resume here.</button>
+            <p className="text-center">For more details, feel free to contact me.</p>
         </section>
     );
 }
